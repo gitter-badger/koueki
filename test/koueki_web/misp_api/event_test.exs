35f37aa8f7047c0b2a4dc1721eb3d22bef1fc916
Hannah Ward <Hannah.ward9001@gmail.com>
2019-02-20 18:00:47 +0000

    new: Add fail-state tests

--- test/koueki_web/misp_api/event_test.exs
+++ test/koueki_web/misp_api/event_test.exs
@@ -12,5 +12,23 @@ defmodule KouekiWeb.MISPAPI.EventsControllerTest do
       |> post("/v1/events/", %{Event: %{info: "my event"}})
 
     assert %{"Event" => %{"info" => "my event"}} = json_response(conn, 201)
+
+    conn = 
+      build_conn()
+      |> assign(:user, user)
+      |> post("/v1/events/", %{Event: %{}})
+
+    assert %{"error" => %{"info" => ["can't be blank"]}} = json_response(conn, 400)
+
+    conn = 
+      build_conn()
+      |> assign(:user, user)
+      |> post("/v1/events/", %{Event: %{info: "yui is best yuru", Attribute: [
+        %{type: "ip-dst", value: "8.8.8.8"}
+      ]}})
+
+    assert %{"Event" => %{"Attribute" => attributes}} = json_response(conn, 201)
+    assert [%{"type" => "ip-dst", "value" => "8.8.8.8", "category" => "Network activity"}] = attributes
+      
   end
 end
